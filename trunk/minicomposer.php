ab49e3c259af8120205b705009ce92dadb6d51c9
shennemann <s.hennemann@rto.de>
2017-06-08 11:37:41 +0200

    Change version

--- trunk/minicomposer.php
+++ trunk/minicomposer.php
@@ -20,7 +20,7 @@ use Inc\MinicomposerDeactivator;
  * Plugin Name:       MiniComposer
  * Plugin URI:        MiniComposer
  * Description:       Layout page builder plugin.
- * Version:           1.4.4
+ * Version:           1.4.5
  * Author:            crazypsycho
  * Author URI:        https://github.com/crazypsycho
  * License:           GPL-2.0+
